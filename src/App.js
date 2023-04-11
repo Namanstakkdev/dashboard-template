178a70894b7bc3e13665818003c7f12bfa17dd55
harmeet.singh <harmeet.singh@antiersolutions.com>
2023-04-11 13:05:57 +0530

    displaying side bar but have to fix design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import './App.css';
 import Loader from "./components/loader/Loader";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <>
-      <Loader />
+      {/* <Loader /> */}
+
+      <div id="app">
+        <aside class="main-sidebar fixed offcanvas b-r sidebar-tabs" data-toggle='offcanvas'>
+          <Sidebar />
+        </aside>
+      </div>
     </>
   );
 }
